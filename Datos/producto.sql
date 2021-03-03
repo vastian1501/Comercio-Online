c1053bb4b02f8d7960809d36e880f7d8dbe04766
Ruben A <ruben.arriazu.simon@gmail.com>
2021-03-03 09:34:55 +0100

    Añade datos

---
+++ Datos/producto.sql
@@ -0,0 +1,32 @@
+insert into producto (nombre, precio, stock, id_vendedor, id_categoria)
+values
+	("Platano", 2.6, 100, 8, 1),
+	("Manzana", 2.5, 30, 15, 1),
+	("Manzana", 1.6, 47, 4, 1),
+	("Naranja", 1.9, 158, 8, 1),
+	("Melocoton", 2.1, 250, 31, 1),
+
+	("Acelga", 1.2, 21, 8, 2),
+	("Espinaca", 1.86, 32, 15, 2),
+	("Espinaca", 1.56, 24, 8, 2),
+	("Coliflor", 1.30, 12, 33, 2),
+	("Pimiento verde", 1.30, 12, 33, 2),
+	("Pimiento verde", 1.67, 7, 11, 2),
+
+	("Queso roncal", 22, 12, 46, 3),
+	("Queso de la mancha", 18, 25, 33, 3),
+	("Queso de la mancha", 16, 23, 16, 3),
+	("Queso roquefort", 25, 8, 13, 3),
+	("Queso oveja", 19, 7, 27, 3),
+
+	("Paulaner", 1.2, 200, 36, 4),
+	("Ginebra", 16, 40, 36, 4),
+	("Ginebra", 17, 20, 5, 4),
+	("Ginebra", 14, 30, 47, 4),
+	("Sidra", 1.6, 70, 47, 4),
+	("Sidra", 1.8, 30, 15, 4),
+	("Jerez", 11, 20, 15, 4),
+	("Jerez", 16, 80, 19, 4),
+
+	("Agua mineral 1L", 0.48, 200, 28, 5),
+	("Fanta limon", 0.7, 40, 28, 5);
--- TiendaOnline.sql
+++ TiendaOnline.sql
@@ -4,36 +4,36 @@ default character set utf8 collate utf8_unicode_ci;
 use TiendaOnline;
 
 create table cliente (
-dni varchar(9) not null primary key,
-nombre varchar(14),
-apellidos varchar(20),
-direccion varchar(60),
-email varchar(80),
-tlfn varchar(12)
+	dni varchar(9) not null primary key,
+	nombre varchar(14),
+	apellidos varchar(20),
+	direccion varchar(60),
+	email varchar(80),
+	tlfn varchar(12)
 )engine=InnoDB;
 
 create table vendedor (
-id_vendedor int(3) not null auto_increment primary key,
-nombre varchar(30),
-direccion varchar(60)
+	id_vendedor int(3) not null auto_increment primary key,
+	nombre varchar(30),
+	direccion varchar(60)
 )engine=InnoDB;
 
 create table categoria (
-id_categoria int(3) not null primary key,
-nombre varchar(14),
-descripcion varchar(40)
+	id_categoria int(3) not null primary key,
+	nombre varchar(14),
+	descripcion varchar(40)
 )engine=InnoDB;
 
 create table producto (
-id_producto int(3) not null auto_increment primary key,
-nombre varchar(14),
-descripcion varchar(40),
-precio int(14),
-stock int(7),
-id_vendedor int(3),
-id_categoria int(3),
-constraint producto_FK foreign key (id_vendedor) references vendedor(id_vendedor),
-constraint producto_FK2 foreign key (id_categoria) references categoria(id_categoria)
+	id_producto int(3) not null auto_increment primary key,
+	nombre varchar(14),
+	descripcion varchar(40),
+	precio int(14),
+	stock int(7),
+	id_vendedor int(3),
+	id_categoria int(3),
+	constraint producto_FK foreign key (id_vendedor) references vendedor(id_vendedor),
+	constraint producto_FK2 foreign key (id_categoria) references categoria(id_categoria)
 )engine=InnoDB;
 
 create table pedido (
