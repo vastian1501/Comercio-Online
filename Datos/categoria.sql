e2b7435954abeeec7e266e71d2b794d412e29039
Ruben A <ruben.arriazu.simon@gmail.com>
2021-03-03 09:04:18 +0100

    Crea bbdd

---
+++ Datos/categoria.sql
@@ -0,0 +1,11 @@
+insert into categoria (nombre, descripción)
+values
+	("Frutas", "Fruta de temporada (o no)"),
+    ("Verduras", "Verduraas frescas"),
+    ("Lacteos", "De la vaca a su casa"),
+    ("Bebidas alcholicas", "Consuma con moderacion"),
+    ("Bebidas no alcholicas", "Para su hidratacion"),
+    ("Belleza", "Siempre bella"),
+    ("Bricolaje", "Para los chapuzas"),
+    ("Material escolar", "Los niños  son el futuro"),
+    ("Electronica", "Televisiones, telefonos y PCs");
